24bcd8ed21af3a0822a16bccb30f754514fe5677
harikarthik-s <harikarthiks15@gmail.com>
2024-04-12 22:45:03 +0530

    Backend AI model connected

---
+++ src/libs/runChat.js
@@ -0,0 +1,49 @@
+const {
+  GoogleGenerativeAI,
+  HarmBlockThreshold,
+  HarmCategory,
+} = require("@google/generative-ai");
+
+const MODEL_NAME = process.env.VITE_PUBLIC_GEMINI_MODEL_NAME;
+const API_KEY = process.env.VITE_PUBLIC_GEMINI_API_KEY;
+
+async function runChat(prompt) {
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 2048,
+  };
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [],
+  });
+  try {
+    const result = await chat.sendMessage(prompt);
+    const response = result.response;
+    return { data: response.text() };
+  } catch (error) {
+    return { error: error.message.split(":").reverse()[0] };
+  }
+}
